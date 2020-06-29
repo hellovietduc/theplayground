fafbc2e0e73d63dfd04351a63e8d4c69c4963e53
vietduc01100001 <vietduc01100001@gmail.com>
2020-06-29 22:45:50 +0700

    add run code endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,18 @@
 const api = require('restana')();
 const bodyParser = require('body-parser');
-const signale = require('signale');
 const runCode = require('./run-code');
+const logger = require('../util/log')('API');
 
 module.exports.init = async config => {
+    api.use(bodyParser.urlencoded({ extended: true }));
     api.use(bodyParser.json());
-    api
-        .post('/code/run/:lang', ...runCode);
+
+    const attachConfig = (req, res, next) => {
+        req.config = config;
+        next();
+    };
+    api.post('/code/run/:lang', attachConfig, ...runCode);
 
     await api.start(config.port);
-    signale.success(`[API] Listening on ${config.port}`);
+    logger.success(`Listening on ${config.port}`);
 };
--- src/api/run-code.js
+++ src/api/run-code.js
@@ -1,15 +1,38 @@
 const { v4: uuidv4 } = require('uuid');
+const codeRunner = require('../service/code-runner');
 
 const validate = (req, res, next) => {
+    const { lang } = req.params;
+    const { code } = req.body;
+    const { supportedLanguages } = req.config.validation;
+
+    if (!supportedLanguages.includes(lang)) {
+        res.body = {
+            error: 'LANG_NOT_SUPPORTED'
+        };
+        return res.send(400);
+    }
+
+    if (!code || !code.trim()) {
+        res.body = {
+            error: 'CODE_EMPTY'
+        };
+        return res.send(400);
+    }
+
+    req.lang = lang;
+    req.code = code.trim();
+
     next();
 };
 
 const runCode = (req, res) => {
+    const { code, lang } = req;
     const id = uuidv4();
-    res.body = {
-        id
-    };
+    res.body = { id };
     res.send();
+
+    codeRunner.run(code, lang, id);
 };
 
 module.exports = [validate, runCode];
