cb6978fa6f4ba326229a1e544377538e0edbf5dc
vietduc01100001 <vietduc01100001@gmail.com>
2020-06-30 17:12:12 +0700

    add get template from api

--- public/index.js
+++ public/index.js
@@ -27,4 +27,44 @@ const runCode = () => {
     consoleStatus.innerText = 'Waiting for remote server...';
 };
 
+const getTemplate = lang => new Promise(resolve => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `http://localhost:3000/template/${lang}`, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    xhr.onreadystatechange = function onStateChange() {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            resolve(this.responseText);
+        }
+    };
+
+    xhr.send();
+});
+
+const onSelectLang = async () => {
+    const curLang = langChooser.value.trim();
+    const code = codeInput.value.trim();
+
+    const prevLang = sessionStorage.getItem('prevLang');
+    sessionStorage.setItem(`lastCode:${prevLang}`, code);
+
+    let lastCode = sessionStorage.getItem(`lastCode:${curLang}`);
+    if (!lastCode) {
+        lastCode = await getTemplate(curLang);
+    }
+    codeInput.value = lastCode;
+    sessionStorage.setItem('prevLang', curLang);
+};
+
+window.onload = async () => {
+    sessionStorage.clear();
+    const lang = langChooser.value.trim();
+    const template = await getTemplate(lang);
+    if (template) {
+        codeInput.value = template;
+        sessionStorage.setItem('prevLang', lang);
+    }
+};
+
 runBtn.addEventListener('click', runCode);
+langChooser.addEventListener('change', onSelectLang);
