ae12ece0bd208e9ca8a6bbe45f231901077f34fe
vietduc01100001 <vietduc01100001@gmail.com>
2020-07-22 23:42:40 +0700

    remove json from script build backend

--- build-back.sh
+++ build-back.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-! command -v json >/dev/null && npm install --global json
-
 VERSION="$(date +"%y.%m%d.%H%M")"
 
 docker --context default build -t docker.pkg.github.com/vietduc01100001/theplayground/theplayground:"$VERSION" backend
